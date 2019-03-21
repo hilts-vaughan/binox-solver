fae9a231c0dff1f9ac51f54f6f39d5549d29dcb7
Vaughan Hilts <hilts.vaughan@gmail.com>
2019-03-20 21:49:40 -0400

    npm: Exported module for NPM

--- dist/index.browser.js
+++ dist/index.browser.js
@@ -1,4 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.BinoxSolver = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 class Point {
   constructor(x, y) {
     this.x = x;
@@ -403,4 +403,5 @@ require('./PuzzleSolutionChecker');
  */
 module.exports = PuzzleSolutionSolver;
 
-},{"./Point":1,"./PuzzleBoard":2,"./PuzzleSlice":3,"./PuzzleSolutionChecker":4,"./PuzzleSolutionSolver":5}]},{},[6]);
+},{"./Point":1,"./PuzzleBoard":2,"./PuzzleSlice":3,"./PuzzleSolutionChecker":4,"./PuzzleSolutionSolver":5}]},{},[6])(6)
+});
