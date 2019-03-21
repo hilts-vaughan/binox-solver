471cf82762c4054302715260e48cab7fbf32aa41
Vaughan Hilts <hilts.vaughan@gmail.com>
2019-03-20 22:08:28 -0400

    npm: More exports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 const PuzzleSolutionSolver = require('./PuzzleSolutionSolver');
-
-// needed in order to get them all bundled together
-require('./Point');
-require('./PuzzleBoard');
-require('./PuzzleSlice');
-require('./PuzzleSolutionChecker');
+const PuzzleBoard = require('./PuzzleBoard');
 
 /**
  * This is the default export since we want to provide one to make the module easier to use.
  * You can still access the internals if you need to by loading the module and requiring files
  * in from the path of it.
  */
-module.exports = PuzzleSolutionSolver;
+module.exports = {
+  solver: PuzzleSolutionSolver,
+  board: PuzzleBoard
+};
